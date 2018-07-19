47616e39ce3d0b99e9e862322c4039aa4e376000
Kirstie Cook <kirstiea@gmail.com>
2018-07-19 14:04:03 -0500

    Updating for meeting on July 23

--- public/index.php
+++ public/index.php
@@ -219,10 +219,11 @@ body {
    On March 26, the newest version of the baseline and credit calculation tool (Maryland Nutrient Tracking Tool or MNTTv.2) will be released on this website.   All existing accounts and projects will be available on the new version. <b>Anyone wishing to use assessments from the current version for submissions to either the Certainty or Agricultural Credit Certification Program will need to re-run them using the latest version since the results will differ. </b> 
   <p>The current version will be archived for those with existing Certainty assessments, and access will be granted upon request to the Maryland Department of Agriculture (MDA). </p>
   <p>For questions or additional information, contact Susan Payne, MDA’s Coordinator of the Ecosystem Markets and Certainty Programs at  <a href="mailto:nutrade.mda@maryland.gov">nutrade.mda@maryland.gov</a>.</p>
-  <h2 class="header_left_content"><center><p>The Maryland Water Quality Trading Advisory<br>Committee to Meet on March 26 </p></center></h2>
+
+  <h2 class="header_left_content"><center><p>The Maryland Water Quality Trading Advisory<br>Committee to Meet on July 23 </p></center></h2>
   <p class="text_left_content">
-    <b>The next meeting of the Trading Advisory Committee will be held on Monday, March 26, from 1:00 to 4:00 p.m., at the Maryland Department of Agriculture’s headquarters, 50 Harry S. Truman Parkway, Annapolis, MD. </b>
-    The Committee acts as an ongoing consultative group to provide direction to the overall trading program and oversee further enhancement of the trading infrastructure. This month’s meeting will focus on updates of the pending trading regulations, Aligning for Growth policies, and the state’s Watershed Implementation Plan.
+    <b>The next meeting of the Trading Advisory Committee will be held on Monday, July 23, from 1:00 to 4:00 p.m., at the Maryland Department of the Environment’s headquarters, 1800 Washington Boulevard, Baltimore, MD 21230. </b>
+    The Committee acts as an ongoing consultative group to provide direction to the overall trading program and oversee further enhancement of the trading infrastructure. This month’s meeting will focus on the new trading regulations and the state’s Watershed Implementation Plan.
   </p>
 </div>
 
